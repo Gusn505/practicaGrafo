9321b170762847440f1ae08fd75f9d13bfac13fe
juxxy <juxxy@DESKTOP-PD9L9MM>
2022-11-28 18:57:27 -0600

    Agregar archivos de proyecto.

---
+++ practicaGrafo/nodo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace practicaGrafo
+{
+    internal class Node
+    {
+    
+
+            public int data;
+            public List<Node> children = new List<Node>();
+            public List<Node> father = new List<Node>();
+            public Node(Graph graph)
+            {
+
+                data = graph.assignNodeData;
+                graph.assignNodeData++;
+            }
+    }
+}
